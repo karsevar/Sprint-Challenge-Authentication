b2fe0e54abf6b9f65774977b49615fec38b06f41
masonkarsevar <masonkarsevar@gmail.com>
2019-09-20 08:32:09 -0700

    register endpoint is working as expected

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,21 @@
 const userDb = require('./authModel.js');
-
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+
+  userDb.add(user) 
+    .then(saved => {
+      res.status(201).json(`record with id ${saved} has been added to the database`)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 });
 
 router.post('/login', (req, res) => {
